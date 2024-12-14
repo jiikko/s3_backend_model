cc83c528a84f85101bd3cd175b06624dcac3cb66
jiikko <n905i.1214@gmail.com>
2024-12-14 23:33:02 +0900

    first commit

---
+++ lib/s3_backend_model.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "s3_backend_model/version"
+
+module S3BackendModel
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/s3_backend_model_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe S3BackendModel do
+  it "has a version number" do
+    expect(S3BackendModel::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
